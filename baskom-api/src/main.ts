7433972b805d6b9215f964f7e12b17bb2e78cbad
Juan Prasetyo <juanprasetyo000@gmail.com>
2024-04-04 17:56:10 +0700

    feat: initial api docs

--- baskom-api/src/main.ts
+++ baskom-api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('BasKom API')
+    .setDescription('API Documentation for BasKom web application')
+    .setVersion('1.0')
+    .addTag('BasKom')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
