90b57866a0b3dc6f6fb5e042f5f6b5bc9b656771
Juan Prasetyo <juanprasetyo000@gmail.com>
2024-05-27 20:16:02 +0700

    chore(server): add logging for BASE_URL on server start

--- baskom-api/server.js
+++ baskom-api/server.js
@@ -4,4 +4,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log('BASE_URL:', process.env.BASE_URL);
 });
