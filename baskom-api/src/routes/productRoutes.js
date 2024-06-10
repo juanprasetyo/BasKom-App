eda5d7dd69fd15c83aebe90b936d0a17571f2d8b
Juan Prasetyo <juanprasetyo000@gmail.com>
2024-06-10 11:01:57 +0700

    feat: add filtering product features

--- baskom-api/src/routes/productRoutes.js
+++ baskom-api/src/routes/productRoutes.js
@@ -3,10 +3,12 @@ const { validate } = require('../helpers/validationHelper');
 const {
   validateProductCreation,
   validateProductUpdate,
+  validateProductSearch,
 } = require('../validations/productValidations');
 const {
   addProductHandler,
   getAllProductsHandler,
+  searchProductsHandler,
   getAllProductsByUserIdHandler,
   getProductByIdHandler,
   updateProductHandler,
@@ -19,6 +21,7 @@ const router = express.Router();
 
 router.post('/products', auth, hasRole('Penjual'), validate(validateProductCreation), addProductHandler);
 router.get('/products', getAllProductsHandler);
+router.get('/products/search', validate(validateProductSearch), searchProductsHandler);
 router.get('/products/user', auth, hasRole('Penjual'), getAllProductsByUserIdHandler);
 router.get('/products/:id', getProductByIdHandler);
 router.put('/products/:id', auth, hasRole('Penjual'), validate(validateProductUpdate), updateProductHandler);
